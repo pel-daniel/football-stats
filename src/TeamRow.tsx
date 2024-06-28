3177470f65c65296185c6bd2523acd50a803de59
Daniel Garcia <pel_daniel@hotmail.com>
2024-06-27 19:58:39 -0700

    style: highlight today & tomorrow matches

--- src/TeamRow.tsx
+++ src/TeamRow.tsx
@@ -1,3 +1,4 @@
+import classNames from "classnames";
 import { Team } from "./apiClient"
 import { Flag } from "./Flag";
 
@@ -5,26 +6,37 @@ import './TeamRow.css';
 import { getScore } from "./tournamentUtils";
 
 export const TeamRow = ({ team }: { team: Team }) => {
+  const today = new Date();
+  const tomorrow = new Date();
+  tomorrow.setDate(today.getDate() + 1);
+  const timezone = today.toLocaleDateString(undefined, {day:'2-digit', timeZoneName: 'short' }).substring(4);
+
   return (
     <div className="flex match-row">
       <Flag team={team} />
       <div className="team-name">{team.name}</div>
       <div className="team-points">{team.points}</div>
+      {/* <div className="team-difference">({team.difference})</div> */}
 
       <div className="match-result-row">
         { team.matches.map(match => {
+          const date = new Date(`${match.date} ${timezone}`);
           const againstTeam = match.team1.code !== team.code ? match.team1 : match.team2;
 
           return (
-            <div className={`match-result match-result-${getScore(match, team)}`}>
+            <div
+              className={classNames(
+                `match-result match-result-${getScore(match, team)}`,
+                {
+                  "match-result-today": today.toDateString() === date.toDateString(),
+                  "match-result-tomorrow": tomorrow.toDateString() === date.toDateString()
+                }
+              )}
+            >
               <Flag team={againstTeam} size="sm" key={againstTeam.code} />
             </div>
           );
         })}
-
-        {/* {team.matchResults.map((result, index) =>
-          <div key={index} className={`match-result match-result-${result}`}></div>
-        )} */}
       </div>
     </div>
   )
