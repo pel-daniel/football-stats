1a0b9fc409a252d52fab6edbed22c570dd52e222
Daniel Garcia <pel_daniel@hotmail.com>
2026-02-03 19:58:46 -0800

    refactor: use css module for TeamRow

--- src/components/TeamRow.tsx
+++ src/components/TeamRow.tsx
@@ -1,9 +1,10 @@
 import classNames from "classnames";
-import { Team } from "../utils/apiClient"
 import { Flag } from "./Flag";
 
-import './TeamRow.css';
+import { Team } from "../utils/apiClient"
 import { getMatchResult } from "../utils/tournamentUtils";
+
+import styles from './TeamRow.module.css';
 
 export const TeamRow = ({ team }: { team: Team }) => {
   const today = new Date();
@@ -12,12 +13,12 @@ export const TeamRow = ({ team }: { team: Team }) => {
   const timezone = today.toLocaleDateString(undefined, {day:'2-digit', timeZoneName: 'short' }).substring(4);
 
   return (
-    <div className="flex match-row">
+    <div className={classNames("flex", styles["match-row"])}>
       <Flag team={team} />
-      <div className="team-name">{team.name}</div>
-      <div className="team-points">{team.points}</div>
+      <div className={styles["team-name"]}>{team.name}</div>
+      <div className={styles["team-points"]}>{team.points}</div>
 
-      <div className="match-result-row">
+      <div className={styles["match-result-row"]}>
         { team.matches.map((match, index) => {
           const date = new Date(`${match.date.replace(/-/g, "/")} ${timezone}`);
           const againstTeam = match.team1.code !== team.code ? match.team1 : match.team2;
@@ -25,15 +26,16 @@ export const TeamRow = ({ team }: { team: Team }) => {
           return (
             <div
               className={classNames(
-                `match-result match-result-${getMatchResult(match, team)}`,
+                styles["match-result"],
+                styles[`match-result-${getMatchResult(match, team)}`],
                 {
-                  "match-result-today": today.toDateString() === date.toDateString(),
-                  "match-result-tomorrow": tomorrow.toDateString() === date.toDateString()
+                  [styles["match-result-today"]]: today.toDateString() === date.toDateString(),
+                  [styles["match-result-tomorrow"]]: tomorrow.toDateString() === date.toDateString()
                 }
               )}
               key={`${againstTeam.code}-${index}`}
             >
-              <div className="match-flag">
+              <div className={styles["match-flag"]}>
                 <Flag team={againstTeam} size="sm" key={againstTeam.code} />
               </div>
             </div>
