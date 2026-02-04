c4bb03d72cbeb4d33930a186c50ca74ef6d28bb5
Daniel Garcia <pel_daniel@hotmail.com>
2026-02-03 19:54:34 -0800

    refactor: use css module for MatchCard

--- src/components/MatchCard.tsx
+++ src/components/MatchCard.tsx
@@ -2,7 +2,7 @@ import { CSSProperties } from "react";
 import { Flag } from "./Flag";
 import { Match } from "../utils/apiClient";
 
-import "./MatchCard.css";
+import styles from "./MatchCard.module.css";
 
 export const MatchCard = ({ match, groupIndex }: { match: Match, groupIndex: number }) => {
   const [fullTimeScore1, fullTimeScore2] = match.score.ft || [undefined, undefined];
@@ -12,10 +12,17 @@ export const MatchCard = ({ match, groupIndex }: { match: Match, groupIndex: num
   const score2 = penaltiesScore2 || extraTimeScore2;
 
   return (
-    <div className="match" style={{ "--color-group": `var(--color-group-${groupIndex})` } as CSSProperties}>
+    <div
+      className={styles["match"]}
+      style={{ "--color-group": `var(--color-group-${groupIndex})` } as CSSProperties}
+    >
       <div className="flex">
         <Flag team={match.team1} />
-        <div className="calendar-team-name">{match.team1.name}</div>
+
+        <div className={styles["team-name"]}>
+          {match.team1.name}
+        </div>
+
         <div>
           {fullTimeScore1}
           {score1 !== undefined && ` (${score1})`}
@@ -24,7 +31,11 @@ export const MatchCard = ({ match, groupIndex }: { match: Match, groupIndex: num
 
       <div className="flex">
         <Flag team={match.team2} />
-        <div className="calendar-team-name">{match.team2.name}</div>
+
+        <div className={styles["team-name"]}>
+          {match.team2.name}
+        </div>
+
         <div>
           {fullTimeScore2}
           {score2 !== undefined && ` (${score2})`}
