8d004ccc8a00b25ee1197eeb378bd8a22491027c
Daniel Garcia <pel_daniel@hotmail.com>
2026-02-03 19:50:13 -0800

    refactor: use css module for GroupCard

--- src/components/GroupCard.tsx
+++ src/components/GroupCard.tsx
@@ -2,20 +2,22 @@ import { CSSProperties } from "react";
 import { Group } from "../utils/apiClient";
 import { TeamRow } from "./TeamRow";
 
-import './GroupCard.css';
+import styles from './GroupCard.module.css';
 
 export const GroupCard = ({ group, index }: { group: Group, index: number }) => {
   return (
     <div style={{ "--color-group": `var(--color-group-${index})` } as CSSProperties}>
-      <h3 className="group-card-name">
+      <h3 className={styles["group-card-name"]}>
         {group.name}
       </h3>
 
-      <div className="group-card">
+      <div className={styles["group-card"]}>
         {
           group.
             teams.
-            sort((team1, team2) => team1.points !== team2.points ? team2.points - team1.points : team2.difference - team1.difference).
+            sort((team1, team2) =>
+              team1.points !== team2.points ? team2.points - team1.points : team2.difference - team1.difference
+            ).
             map(team => <TeamRow team={team} key={team.code} />)
         }
       </div>
